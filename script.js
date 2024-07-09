acfe73fe4003a62b49fdeecea96ae2ad27eec560
Kriti Raj <6517kritiraj@gmail.com>
2024-07-09 22:15:40 +0530

    API changed

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "0b5cb101a2msh4f819749598bdefp1ce794jsn12d1542afa53",
-    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+    "x-rapidapi-key": "0b5cb101a2msh4f819749598bdefp1ce794jsn12d1542afa53",
+    "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
   },
 };
 const getWeather = (city) => {
-  cityName.innerHTML = city;
   fetch(
-    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
+    "https://weatherapi-com.p.rapidapi.com/current.json?q=" + city,
     options
   )
     .then((response) => response.json())
     .then((response) => {
       console.log(response);
-      temp.innerHTML = response.temp;
-      temp2.innerHTML = response.temp;
-      feels_like.innerHTML = response.feels_like;
-      humidity.innerHTML = response.humidity;
-      humidity2.innerHTML = response.humidity;
-      min_temp.innerHTML = response.min_temp;
-      max_temp.innerHTML = response.max_temp;
-      wind_speed.innerHTML = response.wind_speed;
-      wind_degrees.innerHTML = response.wind_degrees;
-      wind_degrees2.innerHTML = response.wind_degrees;
-      sunrise.innerHTML = response.sunrise;
-      sunset.innerHTML = response.sunset;
+      cityName.innerHTML = city;
+      const res = response.current;
+      temp.innerHTML = res.temp_c;
+      temp2.innerHTML = res.temp_c;
+      feels_like.innerHTML = res.feelslike_c;
+      humidity.innerHTML = res.humidity;
+      humidity2.innerHTML = res.humidity;
+      pressure.innerHTML = res.pressure_mb;
+      gust.innerHTML = res.gust_kph;
+      wind_speed.innerHTML = res.wind_kph;
+      wind_degrees.innerHTML = res.wind_degree;
+      wind_degrees2.innerHTML = res.wind_degree;
+      timeZone.innerHTML = response.location.tz_id;
+      lastUpdate.innerHTML = res.last_updated;
+      year.innerHTML = response.location.localtime.slice(0, 4);
     })
-    .catch((err) => console.error(err));
+    .catch((err) => {
+      console.error(err),
+        head.innerHTML = "Please check the spelling of the location you entered.";
+      cityName.innerHTML = "";
+    });
 };
 
 submit.addEventListener("click", (e) => {
